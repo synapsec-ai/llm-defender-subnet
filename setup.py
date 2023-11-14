855abcac2718d004f417eb068eb11589f6ec3adf
ceterum1 <ceterum1@proton.me>
2023-11-14 20:17:18 +0200

    modified setup files

---
+++ setup.py
@@ -0,0 +1,8 @@
+'''
+This minimal setup.py installs the prompt-injection-subnet python
+module. Configuration parameters have been defined in pyproject.toml and
+setup.cfg, see PEP-518 and PEP-517 for more information.
+'''
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
