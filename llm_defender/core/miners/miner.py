202e34c28a410e49457293d1aa8c30ae9ff64077
ceterum1 <ceterum1@proton.me>
2023-12-06 22:14:35 +0200

    enable other engines

--- llm_defender/core/miners/miner.py
+++ llm_defender/core/miners/miner.py
@@ -162,8 +162,8 @@ class PromptInjectionMiner(BaseNeuron):
         # detections. Initializing the engine also executes the engine.
         engines = [
             heuristics.HeuristicsEngine(prompt=synapse.prompt),
-            #text_classification.TextClassificationEngine(prompt=synapse.prompt),
-            #vector_search.VectorEngine(prompt=synapse.prompt, db_path="/tmp/chromadb/"),
+            text_classification.TextClassificationEngine(prompt=synapse.prompt),
+            vector_search.VectorEngine(prompt=synapse.prompt, db_path="/tmp/chromadb/"),
         ]
 
         engine_confidences = []
