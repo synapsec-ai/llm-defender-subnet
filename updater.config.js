8f3785e32e8dcb500a266aef86256e5ca83b994c
ceterum1 <ceterum1@proton.me>
2023-12-07 18:22:07 +0200

    change update interval to every 30 minutes for production use

--- updater.config.js
+++ updater.config.js
@@ -5,7 +5,7 @@ module.exports = {
             interpreter: "/bin/bash",
             script: "./scripts/updater.sh",
             env: {
-                "UPDATE_INTERVAL": 60,
+                "UPDATE_INTERVAL": 1800,
                 "BRANCH": "dev/auto_update_and_prepare_script"
             },
             max_restarts: 5
