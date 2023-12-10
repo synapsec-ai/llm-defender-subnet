e0ecc9fb8e1935d8592a8433cf35006f6ce6c42a
ceterum1 <ceterum1@proton.me>
2023-12-10 18:52:17 +0200

    refactored text-to-sql engine

--- llm_defender/core/miners/engines/prompt_injection/text_to_sql.py
+++ llm_defender/core/miners/engines/prompt_injection/text_to_sql.py
@@ -5,6 +5,7 @@ feature of the llm-defender-subnet.
 from re import sub
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import bittensor as bt
+from os import path, makedirs
 
 from llm_defender.base.engine import BaseEngine
 
@@ -26,102 +27,100 @@ class TextToSqlEngine(BaseEngine):
         super().__init__(name=name)
 
         self.prompt = prompt
+        self.keywords = {
+            "CREATE": 1.0,
+            "DELETE": 1.0,
+            "DROP": 1.0,
+            "INSERT": 1.0,
+            "INTO": 1.0,
+            "TRUNCATE": 1.0,
+            "UPDATE": 1.0,
+        }
+
+        # Ensure the keywords are in lowercase
+        self.keywords = {key.lower(): value for key, value in self.keywords.items()}
 
     def _calculate_confidence(self):
-        pass
+        """
+        The standard implementation looks for a match in the list of
+        keywords and if match is found, the output value of this
+        function is set to the value. The highest value is kept and
+        returned as float.
 
-    def _populate_data(self):
-        pass
+        In order to properly fine-tune the engine, you should also
+        fine-tune the scoring algorithm.
+        """
+
+        if self.output["outcome"] == "converted":
+            scores = [self.keywords.get(string, None) for string in self._clean()]
+
+            return max((score for score in scores if score is not None), default=0.5)
+
+        return 0.5
+
+    def _populate_data(self, results):
+        if results:
+            return {
+                "outcome": "converted",
+                "data": f"{results[len(self.prompt) :].strip()}",
+            }
+        return {"outcome": "notConverted"}
 
     def prepare(self):
-        pass
+        # Check cache directory
+        if not path.exists(self.cache_dir):
+            try:
+                makedirs(self.cache_dir)
+            except OSError as e:
+                raise OSError(f"Unable to create cache directory: {e}") from e
+
+        _, _ = self.initialize()
+
+        return True
 
     def initialize(self):
-        pass
-
-    def execute(self):
-        pass
-
-    class SqlSubEngine(HeuristicsSubEngine):
-        """
-        This engine is used by the llm-defender-subnet miners for
-        analyzing prompt-injection strings that have been formatted as SQL
-        queries.
-
-        The purpose is to identity potential prompt injections that can be
-        modeled as SQL queries by using static analysis of the resulting SQL
-        query.
-
-        """
-
-        def __init__(self, prompt: str, weight: float, name: str = "text-to-sql"):
-            super().__init__(prompt=prompt, name=name, weight=weight)
-
-            self.keywords = {
-                "CREATE": 1.0,
-                "DELETE": 1.0,
-                "DROP": 1.0,
-                "INSERT": 1.0,
-                "INTO": 1.0,
-                "TRUNCATE": 1.0,
-                "UPDATE": 1.0,
-            }
-
-            # Ensure the keywords are in lowercase
-            self.keywords = {key.lower(): value for key, value in self.keywords.items()}
-
-        def invoke(self) -> bool:
-            self.output = self.convert()
-            self.confidence = self.scoring()
-
-            return True
-
-        def convert(self) -> str:
+        try:
             model = GPT2LMHeadModel.from_pretrained(
                 "rakeshkiriyath/gpt2Medium_text_to_sql"
             )
             tokenizer = GPT2Tokenizer.from_pretrained(
                 "rakeshkiriyath/gpt2Medium_text_to_sql"
             )
+        except Exception as e:
+            raise Exception(f"Error occurred while initializing the models: {e}") from e
 
-            input_tensor = tokenizer.encode(self.prompt, return_tensors="pt")
+        return model, tokenizer
 
-            output = model.generate(
-                input_tensor,
-                max_length=256,
-                num_return_sequences=1,
-                pad_token_id=tokenizer.eos_token_id,
-            )
+    def execute(self, model, tokenizer):
+        input_tensor = tokenizer.encode(self.prompt, return_tensors="pt")
 
-            decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+        output = model.generate(
+            input_tensor,
+            max_length=256,
+            num_return_sequences=1,
+            pad_token_id=tokenizer.eos_token_id,
+        )
 
-            return decoded_output[len(self.prompt) :].strip()
+        decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
 
-        def clean(self) -> list:
-            """
-            This method returns a clean list of the prompt given as an
-            argument for the class.
-            """
+        self.output = self._populate_data(decoded_output)
+        self.confidence = self._calculate_confidence()
 
-            # Remove non-alphanumeric characters and extra whitespaces
-            clean_prompt = sub(r"[^a-zA-Z0-9 ]", "", self.output)
-            clean_prompt = sub(r"\s+", " ", clean_prompt).strip()
+        bt.logging.debug(
+            f"Text-to-SQL engine executed (Confidence: {self.confidence} - Output: {self.output})"
+        )
+        return True
 
-            # Convert the individual words into lower-case and return them in a list to be
-            # processed further.
-            return [word.lower() for word in clean_prompt.split()]
+    def _clean(self) -> list:
+        """
+        This method returns a clean list of the prompt given as an
+        argument for the class.
+        """
 
-        def scoring(self) -> float:
-            """
-            The standard implementation looks for a match in the list of
-            keywords and if match is found, the output value of this
-            function is set to the value. The highest value is kept and
-            returned as float.
+        # Remove non-alphanumeric characters and extra whitespaces
+        clean_prompt = sub(r"[^a-zA-Z0-9 ]", "", self.output["data"])
+        clean_prompt = sub(r"\s+", " ", clean_prompt).strip()
 
-            In order to properly fine-tune the engine, you should also
-            fine-tune the scoring algorithm.
-            """
-
-            scores = [self.keywords.get(string, None) for string in self.clean()]
-
-            return max((score for score in scores if score is not None), default=0.5)
+        # Convert the individual words into lower-case and return them in a list to be
+        # processed further.
+        return [word.lower() for word in clean_prompt.split()]
