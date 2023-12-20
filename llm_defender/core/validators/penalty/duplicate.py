d7b47233c793aa54daea1fd4f2aa10ba2fe94623
ceterum1 <ceterum1@proton.me>
2023-12-20 07:34:28 +0200

    revert changes to duplicate function

--- llm_defender/core/validators/penalty/duplicate.py
+++ llm_defender/core/validators/penalty/duplicate.py
@@ -5,17 +5,25 @@ def _calculate_duplicate_percentage(uid, miner_responses, engine, penalty_name="
     penalty = 0.0
     # Isolate engine-specific data
     engine_data = [entry for item in miner_responses for entry in item.get('engine_data', []) if entry.get('name') == engine]
-
     if not engine_data:
         return penalty
     
     # Calculate duplicate percentage
-    duplicate_percentage = 1 - len(set(str(entry) for entry in engine_data)) / len(engine_data)
+    engine_data_str = [str(entry) for entry in engine_data]
+    duplicates = {entry: engine_data_str.count(entry) for entry in engine_data_str}
+    if not duplicates:
+        return penalty
+    duplicate_percentage = ((len(engine_data) - len(duplicates)) / len(engine_data))
 
-    if engine == "engine:yara" and duplicate_percentage > 0.95:
-        penalty += 1
-    elif engine == "engine:vector_search" and duplicate_percentage > 0.15:
-        penalty += 2
+    if not duplicate_percentage:
+        return penalty
+
+    if engine == "engine:yara":
+        if duplicate_percentage > 0.95:
+            penalty += 1
+    elif engine == "engine:vector_search":
+        if duplicate_percentage > 0.15:
+            penalty += 2
     elif engine == "engine:text_classification":
         if duplicate_percentage > 0.5:
             if duplicate_percentage > 0.95:
@@ -26,29 +34,21 @@ def _calculate_duplicate_percentage(uid, miner_responses, engine, penalty_name="
                 penalty += 1
             else:
                 penalty += 0.5
-
     bt.logging.trace(f"Applied penalty score '{penalty}' from rule '{penalty_name}' for UID: '{uid}'. Duplicate % for {engine}: {duplicate_percentage}")
+    
     return penalty
-
 
 def _find_identical_reply(uid, miner_responses, response, engine, penalty_name="Identical replies"):
     """Applies penalty if identical replies are found"""
     penalty = 0.0
-    engine_responses = [data for data in response.get("engines", []) if data["name"] == engine]
-    
-    if not engine_responses:
+    engine_response = [data for data in response["engines"] if data["name"] == engine]
+    if not engine_response:
         return penalty
-    
-    # Extracting the response for comparison
-    response_data = set(entry["data"] for entry in engine_responses)
-    
-    for item in miner_responses:
-        for entry in item.get('engine_data', []):
-            if entry.get('name') == engine and entry.get('data') in response_data:
-                penalty += 0.5
-                break  # Exit loop if duplicate is found
-    
-    bt.logging.trace(f"Applied penalty score '{penalty}' from rule '{penalty_name}' for UID: '{uid}'")
+    if len(engine_response) > 0:
+        if any(engine_response == entry for item in miner_responses for entry in item.get('engine_data', [])):
+            penalty += 0.5
+        
+        bt.logging.trace(f"Applied penalty score '{penalty}' from rule '{penalty_name}' for UID: '{uid}'")
     return penalty
 
 
