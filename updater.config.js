891b60897f12e66de619501b9524fc8c1684c7b0
ceterum1 <150546921+ceterum1@users.noreply.github.com>
2023-12-21 19:23:50 +0200

    Update updater.config.js

--- updater.config.js
+++ updater.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [
         {
-            name: "llm-defender-subnet-updater-dev",
+            name: "llm-defender-subnet-updater",
             interpreter: "/bin/bash",
             script: "./scripts/updater.sh",
             env: {
