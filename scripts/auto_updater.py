e858567d8a662300cdc8670d0fd8dd49214eac02
ceterum1 <150546921+ceterum1@users.noreply.github.com>
2023-12-26 15:41:46 +0200

    Neuron launch script refactoring (#16)
    * Refactored neuron launch script to increase the reliability of the auto-updater

---
+++ scripts/auto_updater.py
@@ -0,0 +1,61 @@
+from argparse import ArgumentParser
+import logging
+from time import sleep
+import git
+import subprocess
+import random
+
+logging.basicConfig(level=logging.INFO)
+
+def run(args):
+    """Monitors the given git branch for updates and restart given PM2
+    instances if updates are found"""
+
+    while(True):
+        try:
+            # Setup git repository objects
+            repo = git.Repo()
+            origin = repo.remotes.origin
+
+            # Fetch updates from the remote registry
+            origin.fetch()
+
+            # Check if there are changes in the remote repository
+            if repo.refs[args.branch].commit != repo.refs[f'origin/{args.branch}'].commit:
+                logging.info('Changes detected in remote branch: %s', args.branch)
+                logging.info('Pulling remote branch: %s', args.branch)
+                origin.pull(args.branch)
+                repo.git.checkout(args.branch)
+                logging.info('Checked out to branch: %s', args.branch)
+
+                # Restart pm2 instances
+                for instance_name in args.pm2_instance_names:
+                    try:
+                        sleep_duration = random.randint(1,60)
+                        logging.info('Sleeping for %s seconds before restart', sleep_duration)
+                        sleep(sleep_duration)
+                        subprocess.run(f'git checkout {args.branch} && pm2 restart {instance_name}', check=True, shell=True)
+                        logging.info('Restarted PM2 process: %s', instance_name)
+                    except subprocess.CalledProcessError as e:
+                        logging.error('Unable to restart PM2 instance: %s', e)
+                
+                logging.info('All processes have been restarted')
+            else:
+                logging.info('No changes detected in remote branch: %s', args.branch)
+
+            logging.info('Sleeping for %s seconds.', args.update_interval)
+            sleep(int(args.update_interval))
+        except Exception as e:
+            logging.error('Error occurred: %s', e)
+            raise Exception from e
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument("--pm2_instance_names", nargs='+', help="List of PM instances to keep up-to-date")
+    parser.add_argument("--branch", action="store", help="Git branch to monitor")
+    parser.add_argument("--repo", action="store", help="Git repository address")
+    parser.add_argument("--update_interval", action="store", help="Interval to check for any new updates")
+
+    args = parser.parse_args()
+    run(args)
