133cc51e209bbabb1147906f261c5572d838e447
ceterum1 <ceterum1@proton.me>
2024-02-11 10:45:56 +0200

    added tests and error handling to sign/validation methods

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import llm_defender.base.utils as utils
-
+import bittensor as bt
+import pytest
 
 def test_uid_validation():
     valid_uids = range(0, 256)
@@ -117,4 +118,30 @@ def test_response_data_validation():
         engine_response = value
         assert utils.validate_response_data(engine_response=engine_response) is False
 
+def test_signatures_and_validation():
+    # Setup
+    wallet = bt.wallet(name="test_coldkey", hotkey="test_hotkey").create_if_non_existent(coldkey_use_password=False, hotkey_use_password=False)
 
+    # Valid combinations
+    data = "foobar"
+    signature = utils.sign_data(data=data, wallet=wallet)
+    assert utils.validate_signature(hotkey=wallet.hotkey.ss58_address, data=data, signature=signature) is True
+
+    assert utils.validate_signature(hotkey="5EjwQEJtbhcbZmWtpTVhQV8NGbYULS6djErtLbH8EJY1v9vg", data=data, signature="524e982b34b15fc96f680878c5c7ab38b784a1f49c80316f505b1f440a2f2e121e3a5f4aa23d373b009ec85a191e2de3a3ed0c6d3355a591ed5e841803100687") is True
+
+    # Invalid combinations
+    for _,entry in enumerate([True, False, 1, "foo", [], {}, "a293f9502c45fff1460f451f3c43472824300beb947fe595502fc5ff8e48b7592349b89d08cc170852120f69141e9c9425200ba104b11c850b710e7ee64e2a88", "5EjwQEJtbhcbZmWtpTVhQV8NGbYULS6djErtLbH8EJY1v9vg", {"foo": "bar"}, 0.0, -1, 0]):
+        assert utils.validate_signature(hotkey=entry, data=data, signature=signature) is False
+        assert utils.validate_signature(hotkey=wallet.hotkey.ss58_address, data=entry, signature=signature) is False
+        assert utils.validate_signature(hotkey=wallet.hotkey.ss58_address, data=data, signature=entry) is False
+        assert utils.validate_signature(hotkey=entry, data=entry, signature=entry) is False
+        assert utils.validate_signature(hotkey=wallet.hotkey.ss58_address, data=entry, signature=entry) is False
+        assert utils.validate_signature(hotkey=entry, data=entry, signature=signature) is False
+        assert utils.validate_signature(hotkey=entry, data=data, signature=signature) is False
+
+        with pytest.raises(AttributeError):
+            utils.sign_data(data=data, wallet=entry)
+    
+    for _,entry in enumerate([True, False, 1, [], {}, {"foo": "bar"}, [1,2], ["foo", "bar"]]):
+        with pytest.raises(AttributeError):
+                utils.sign_data(data=entry, wallet=wallet)
