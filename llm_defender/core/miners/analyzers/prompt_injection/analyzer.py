5161c6bfe4ba972aabeb4e125bc35239b1e63a27
ceterum1 <ceterum1@proton.me>
2024-02-23 21:59:51 +0200

    merged release/0.3.0

--- llm_defender/core/miners/analyzers/prompt_injection/analyzer.py
+++ llm_defender/core/miners/analyzers/prompt_injection/analyzer.py
@@ -1,11 +1,14 @@
 """Analyzer method for the prompt injection analyzer"""
+import time
 import bittensor as bt
-
 from llm_defender.base.protocol import LLMDefenderProtocol
 from llm_defender.core.miners.analyzers.prompt_injection.yara import YaraEngine
 from llm_defender.core.miners.analyzers.prompt_injection.text_classification import TextClassificationEngine
 from llm_defender.core.miners.analyzers.prompt_injection.vector_search import VectorEngine
-from llm_defender.base.utils import sign_data
+from llm_defender.base.utils import sign_data, wandb_available
+
+if wandb_available():
+    import wandb
 
 class PromptInjectionAnalyzer:
     """This class is responsible for handling the analysis for prompt injection
@@ -33,7 +36,7 @@ class PromptInjectionAnalyzer:
     
     """
 
-    def __init__(self, wallet: bt.wallet, subnet_version: int):
+    def __init__(self, wallet: bt.wallet, subnet_version: int, args):
         # Parameters
         self.wallet = wallet
         self.subnet_version = subnet_version
@@ -42,6 +45,18 @@ class PromptInjectionAnalyzer:
         self.chromadb_client = VectorEngine().initialize()
         self.model, self.tokenizer = TextClassificationEngine().initialize()
         self.yara_rules = YaraEngine().initialize()
+
+        # Wandb
+        self.wandb_available = wandb_available()        
+
+        if args.use_wandb == 'True':
+            self.use_wandb = True
+        else:
+            self.use_wandb = False
+
+        if self.use_wandb and self.wandb_available:
+            self.wandb_project = args.wandb_project 
+            self.wandb_entity = args.wandb_entity
     
     def execute(self, synapse: LLMDefenderProtocol) -> dict:
         # Responses are stored in a dict
@@ -79,6 +94,22 @@ class PromptInjectionAnalyzer:
 
         # Generate signature for the response
         output["signature"] = sign_data(self.wallet, output["synapse_uuid"])
+
+        # Wandb logging
+        if self.use_wandb and self.wandb_available:
+            try:
+                wandb_logs = [
+                    {"YARA Confidence":yara_response['confidence']},
+                    {"Text Classification Confidence":text_classification_response['confidence']},
+                    {"Vector Search Confidence":vector_response['confidence']},
+                    {"Total Confidence":output['confidence']}
+                ]   
+                log_timestamp = int(time.time())
+                for wl in wandb_logs:
+                    wandb.log(wl, step=log_timestamp)    
+                bt.logging.trace(f"Wandb logs added: {wandb_logs}")
+            except:
+                bt.logging.trace("Wandb logging failed for engine confidences.")
         
         synapse.output = output
 
