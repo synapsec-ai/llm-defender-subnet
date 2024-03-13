cd2fb68e452e4fd5fb0ec15ee0c7f374d813fc43
Juan Jimenez <juan@synapsec.ai>
2024-03-13 17:40:11 +0000

    Include new engine into scripts

--- scripts/prep.py
+++ scripts/prep.py
@@ -2,19 +2,28 @@
 This script prepares the engines before miner is executed.
 """
 import sys
+
+from llm_defender.core.miners.analyzers.prompt_injection.text_classification import (
+    TextClassificationEngine
+)
 from llm_defender.core.miners.analyzers.sensitive_information.yara import YaraEngine
-from llm_defender.core.miners.analyzers.sensitive_information.text_classification import TextClassificationEngine
+from llm_defender.core.miners.analyzers.sensitive_information.text_classification import (
+    TextClassificationEngine
+)
 from llm_defender.core.miners.analyzers.prompt_injection.vector_search import VectorEngine
+
 
 def prepare_engines():
     """Prepare the engines"""
-    # Prepare text classification engine
-
+    # Prepare text classification engines
     if not TextClassificationEngine().prepare():
-        print("Unable to prepare text classification engine")
+        print("Unable to prepare text classification engine for prompt injection")
         sys.exit(1)
 
-    print("Prepared Text Classification engine")
+    if not TextClassificationEngine().prepare():
+        print("Unable to prepare text classification engine for sensitive information")
+        sys.exit(1)
+    print("Prepared Text Classification engines")
 
     # Prepare vector search engine
     if not VectorEngine().prepare():
