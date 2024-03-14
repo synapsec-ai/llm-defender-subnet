fd15bea9d040af08d8fdfd8546bd6c4df4edafa0
Juan Jimenez <juan@synapsec.ai>
2024-03-14 20:01:32 +0000

    Fix prep script

--- scripts/prep.py
+++ scripts/prep.py
@@ -4,11 +4,11 @@ This script prepares the engines before miner is executed.
 import sys
 
 from llm_defender.core.miners.analyzers.prompt_injection.text_classification import (
-    TextClassificationEngine
+    TextClassificationEngine as PromptTextClassEngine
 )
 from llm_defender.core.miners.analyzers.sensitive_information.yara import YaraEngine
 from llm_defender.core.miners.analyzers.sensitive_information.text_classification import (
-    TextClassificationEngine
+    TextClassificationEngine as SensitiveClassEngine
 )
 from llm_defender.core.miners.analyzers.prompt_injection.vector_search import VectorEngine
 
@@ -16,11 +16,11 @@ from llm_defender.core.miners.analyzers.prompt_injection.vector_search import Ve
 def prepare_engines():
     """Prepare the engines"""
     # Prepare text classification engines
-    if not TextClassificationEngine().prepare():
+    if not PromptTextClassEngine().prepare():
         print("Unable to prepare text classification engine for prompt injection")
         sys.exit(1)
 
-    if not TextClassificationEngine().prepare():
+    if not SensitiveClassEngine().prepare():
         print("Unable to prepare text classification engine for sensitive information")
         sys.exit(1)
     print("Prepared Text Classification engines")
