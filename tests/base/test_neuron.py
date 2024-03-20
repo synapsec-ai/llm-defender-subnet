e1f621b4de2742c7e62b645252f2a827668b08cc
Juan Jimenez <juan@synapsec.ai>
2024-03-20 20:49:16 +0000

    Test load_used_nonces if file doesn't exist

--- tests/base/test_neuron.py
+++ tests/base/test_neuron.py
@@ -1,23 +1,34 @@
 from argparse import ArgumentParser
+from typing import List, Any
 from unittest.mock import patch
 
 import bittensor as bt
+import pytest
 
 from llm_defender.base.neuron import BaseNeuron
 
 
-def test_config():
+@pytest.fixture
+def neuron_instance() -> BaseNeuron:
+    # Create a BaseNeuron instance for testing
     parser = ArgumentParser()
-    neuron = BaseNeuron(parser=parser, profile="test")
+    return BaseNeuron(parser, profile="test")
 
-    with patch('os.path.exists', return_value=True):
-        config = neuron.config(bt_classes=[bt.MockSubtensor, bt.MockWallet])
+
+def test_config(neuron_instance: BaseNeuron):
+    with patch("os.path.exists", return_value=True):
+        config = neuron_instance.config(bt_classes=[bt.MockSubtensor, bt.MockWallet])
 
     assert config is not None
 
 
-def test_validate_nonce():
-    parser = ArgumentParser()
-    neuron = BaseNeuron(parser=parser, profile="test")
-    assert neuron.validate_nonce("abc")
-    assert not neuron.validate_nonce("abc")
+def test_validate_nonce(neuron_instance: BaseNeuron):
+    assert neuron_instance.validate_nonce("abc")
+    assert not neuron_instance.validate_nonce("abc")
+
+
+def test_load_used_nonces_file_does_not_exist(neuron_instance: BaseNeuron):
+    with patch("os.path.exists") as mock_exists:
+        mock_exists.return_value = False
+        neuron_instance.load_used_nonces()
+        assert neuron_instance.used_nonces == []
