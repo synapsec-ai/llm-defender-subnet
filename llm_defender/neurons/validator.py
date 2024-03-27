18611de83658ccd807341c43cd36096c31215b5a
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-03-27 11:44:33 +0900

    Undid testing changes

--- llm_defender/neurons/validator.py
+++ llm_defender/neurons/validator.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max_targets",
         type=int,
-        default=1,
+        default=64,
         help="Sets the value for the number of targets to query at once",
     )
 
