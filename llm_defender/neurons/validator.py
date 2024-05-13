a2a4063163bff3a069c47c2a8dee23f98ed2b811
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-05-14 01:50:35 +0900

    undid temp testing changes

--- llm_defender/neurons/validator.py
+++ llm_defender/neurons/validator.py
@@ -395,7 +395,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max_targets",
         type=int,
-        default=8,
+        default=64,
         help="Sets the value for the number of targets to query at once",
     )
 
