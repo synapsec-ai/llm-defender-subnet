3517f280a2f466bd98e371bebcdc66ea96a86da8
ceterum1 <ceterum1@proton.me>
2024-06-15 19:31:18 +0300

    remove frozen flag

--- llm_defender/base/protocol.py
+++ llm_defender/base/protocol.py
@@ -2,18 +2,18 @@ from typing import List, Dict
 import bittensor as bt
 import pydantic
 
+
 class MetricsProtocol(bt.Synapse):
     """This class implements the Synapse responsible for the
     feedback-loop back towards the miner after processing the response
     received from the miner"""
-    
+
     # This variable contains the metrics to be sent back to the Miner
     response_object: Dict
 
     synapse_uuid: str = pydantic.Field(
         ...,
         description="Synapse UUID provides a unique identifier for the prompt sent out by the validator",
-
     )
 
     synapse_nonce: str = pydantic.Field(
@@ -50,28 +50,24 @@ class SubnetProtocol(bt.Synapse):
         ...,
         description="Synapse UUID provides a unique identifier for the prompt sent out by the validator",
         allow_mutation=False,
-        frozen=False
     )
 
     synapse_nonce: str = pydantic.Field(
         ...,
         description="Synapse nonce provides a unique identifier for the prompt sent out by the validator",
         allow_mutation=False,
-        frozen=False
     )
 
     synapse_timestamp: str = pydantic.Field(
         ...,
         description="Synapse timestamp provides a unique identifier for the prompt sent out by the validator",
         allow_mutation=False,
-        frozen=False
     )
 
     subnet_version: int = pydantic.Field(
         ...,
         description="Subnet version provides information about the subnet version the Synapse creator is running at",
         allow_mutation=False,
-        frozen=False
     )
 
     analyzer: str | None = pydantic.Field(
@@ -79,7 +75,6 @@ class SubnetProtocol(bt.Synapse):
         title="analyzer",
         description="The analyzer field provides instructions on which Analyzer to execute on the miner",
         allow_mutation=False,
-        frozen=False
     )
 
     synapse_signature: str = pydantic.Field(
@@ -87,7 +82,6 @@ class SubnetProtocol(bt.Synapse):
         title="synapse_signature",
         description="The synapse_signature field provides the miner means to validate the origin of the Synapse",
         allow_mutation=False,
-        frozen=False
     )
 
     synapse_prompts: List[str] | None = pydantic.Field(
@@ -95,7 +89,6 @@ class SubnetProtocol(bt.Synapse):
         title="synapse_prompt",
         description="Optional field providing additional prompt information.",
         allow_mutation=False,
-        frozen=False
     )
 
     synapse_hash: str | None = pydantic.Field(
@@ -103,7 +96,6 @@ class SubnetProtocol(bt.Synapse):
         title="synapse_hash",
         description="Optional field providing hash information for the synapse.",
         allow_mutation=False,
-        frozen=False
     )
 
     def deserialize(self) -> bt.Synapse:
