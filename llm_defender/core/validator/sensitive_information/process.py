912c8ff4e898f04edc6d5a900fcd447168f27705
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-06-21 15:25:02 +0900

    penalty update

--- llm_defender/core/validator/sensitive_information/process.py
+++ llm_defender/core/validator/sensitive_information/process.py
@@ -330,16 +330,14 @@ def get_response_penalties(validator, response, hotkey, target):
     distance_penalty_multiplier = 1.0
     speed_penalty = 1.0
 
-    if base_penalty >= 20:
+    if sum([base_penalty, false_positive_penalty]) >= 20:
         distance_penalty_multiplier = 0.0
-    elif base_penalty > 0.0:
-        distance_penalty_multiplier = 1 - ((base_penalty / 2.0) / 10)
+    elif sum([base_penalty, false_positive_penalty]) > 0.0:
+        distance_penalty_multiplier = 1 - (sum([base_penalty, false_positive_penalty]) / 20.0)
 
-    if sum([false_positive_penalty, duplicate_penalty]) >= 20:
+    if duplicate_penalty >= 20:
         speed_penalty = 0.0
-    elif sum([false_positive_penalty, duplicate_penalty]) > 0.0:
-        speed_penalty = 1 - (
-            ((sum([false_positive_penalty, duplicate_penalty])) / 2.0) / 10
-        )
+    elif duplicate_penalty > 0.0:
+        speed_penalty = 1 - (duplicate_penalty / 20.0)
 
     return distance_penalty_multiplier, speed_penalty
