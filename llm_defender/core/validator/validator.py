013fef3e4109709e5e93fff31c95075fed310812
m4k1-dev <uzum4k1.uzum4k1@gmail.com>
2024-06-26 22:28:04 +0900

    temp logging changes

--- llm_defender/core/validator/validator.py
+++ llm_defender/core/validator/validator.py
@@ -459,6 +459,7 @@ class SubnetValidator(LLMDefenderBase.BaseNeuron):
 
     def check_hotkeys(self):
         """Checks if some hotkeys have been replaced in the metagraph"""
+        bt.logging.debug(f"validator.hotkeys type: {type(self.hotkeys)}")
         if np.any(self.hotkeys):
             # Check if known state len matches with current metagraph hotkey length
             if len(self.hotkeys) == len(self.metagraph.hotkeys):
